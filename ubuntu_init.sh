7345e25728428dc2b2a81a2f0a9d7174da6c759e
Ivan Camilo Vásquez <ivancamilov@gmail.com>
2015-11-25 09:02:03 -0500

    added vim

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -1,6 +1,7 @@
-
+# Install essential stuff
 sudo apt-get update
-sudo apt-get install -y build-essential git curl
+sudo apt-get install -y build-essential git curl vim
+
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 nvm install 4.2.2
