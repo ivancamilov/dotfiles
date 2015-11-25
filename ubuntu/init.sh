95d795ad3fefc96543af61562adafbfeb7019ec6
Ivan Camilo Vásquez <ivancamilov@gmail.com>
2015-11-25 09:22:03 -0500

    added a few essential packages

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -2,6 +2,9 @@
 sudo apt-get update
 sudo apt-get install -y build-essential git curl vim
 
-
+# Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 nvm install 4.2.2
+
+# Install common Node packages
+npm install ionic bower cordova gulp -g
