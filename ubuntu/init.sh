08e5533bb278cd79295934e1ee543be81780e3d9
Ivan Camilo Vásquez <ivancamilov@gmail.com>
2015-11-25 10:43:09 -0500

    added mongo instalation

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,6 +1,6 @@
 # Install essential stuff
 sudo apt-get update
-sudo apt-get install -y build-essential git curl vim tmux
+sudo apt-get install -y build-essential git curl vim tmux mongodb
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
