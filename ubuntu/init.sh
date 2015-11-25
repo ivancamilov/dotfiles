ba02f26b89eb55c49c2d6c53e0b02b962be03352
Ivan Camilo Vásquez <ivancamilov@gmail.com>
2015-11-25 17:15:35 -0500

    added some stuff required for android's SDK

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,6 +1,6 @@
 # Install essential stuff
 sudo apt-get update
-sudo apt-get install -y build-essential git curl vim tmux unzip mongodb openjdk-7-jdk
+sudo apt-get install -y build-essential git curl vim tmux unzip mongodb openjdk-7-jdk lib32stdc++6 lib32z1
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
