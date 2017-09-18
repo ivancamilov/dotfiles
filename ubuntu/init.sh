3d530aeb28c061b091d5a43840ea84a3ca837fd4
Ivan Vasquez <ivancamilov@gmail.com>
2017-09-17 22:15:53 -0500

    just some formatting changes

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 # Install essential stuff
 sudo apt-get update
 sudo apt-get install -y build-essential git curl vim tmux unzip mongodb openjdk-7-jdk lib32stdc++6 lib32z1
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-nvm install 4.2.2
+nvm install latest
 
 # Install common Node packages
-npm install ionic bower cordova gulp supervisor -g
+npm install ionic bower cordova gulp gulp-cli supervisor -g
